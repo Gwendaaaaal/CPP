a7a14c060554e2ab1d776f34092ca9721422bf57
Gwendal - <gwendaaaaaaal@gmail.com>
2026-01-02 20:39:11 +0100

    beginning of cpp06

---
+++ CPP06/ex00/ScalarConverter.cpp
@@ -0,0 +1,10 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::~ScalarConverter() {}
+
+void ScalarConverter::convert(const std::string &literal)
+{
+	
+}
