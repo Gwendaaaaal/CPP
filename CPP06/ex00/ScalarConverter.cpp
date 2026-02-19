1d813dbc4fbc8a1b31385a188a8df98de4a01632
Gwendal <gholloco@student.42.fr>
2026-02-19 12:07:42 +0100

    Made ScalarConverter an orthodox class in case

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -80,6 +80,17 @@ static void	printInvalid(void)
 
 ScalarConverter::ScalarConverter() {}
 
+ScalarConverter::ScalarConverter(const ScalarConverter &other)
+{
+	(void) other;
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other)
+{
+	(void) other;
+	return *this;
+}
+
 ScalarConverter::~ScalarConverter() {}
 
 void ScalarConverter::convert(const std::string &literal)
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -6,6 +6,8 @@
 class ScalarConverter {
 private:
 	ScalarConverter();
+	ScalarConverter(const ScalarConverter &other);
+	ScalarConverter& operator=(const ScalarConverter &other);
 	~ScalarConverter();
 
 public:
